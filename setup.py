d8150f4471bcf1f842be792e7ad7e7aec1d26a66
Stephan Richter <stephan.richter@gmail.com>
2013-02-28 11:16:49 -0500

    Back to development: 2.0.1

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def alltests():
     return unittest.TestSuite(suites)
 
 setup(name = 'zope.ramcache',
-      version='2.0.0',
+      version='2.0.1.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope RAM Cache',
