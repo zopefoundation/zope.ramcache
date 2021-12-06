edccd184bb86ba6ca10a628c3a1451bd476b47c6
Michael Howitz <mh@gocept.com>
2021-12-06 08:34:23 +0100

    Lint the code.
    Add support for Python 3.8, 3.9 and 3.10.
    Drop support for Python 3.4.

--- setup.py
+++ setup.py
@@ -16,9 +16,11 @@
 import os
 from setuptools import setup, find_packages
 
+
 def read(*rnames):
     with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
         return f.read()
+
 
 TESTS_REQUIRE = [
     'zope.testing',
@@ -45,10 +47,12 @@ setup(name='zope.ramcache',
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
           'Programming Language :: Python :: Implementation :: CPython',
           'Programming Language :: Python :: Implementation :: PyPy',
           'Natural Language :: English',
@@ -72,4 +76,4 @@ setup(name='zope.ramcache',
       },
       include_package_data=True,
       zip_safe=False,
-)
+      )
