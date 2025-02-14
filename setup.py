00ff60df3fc8668ffc43b0fe475e26071e6aa39b
Michael Howitz <icemac@gmx.net>
2025-02-14 09:43:52 +0100

    Preparing release 3.1

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ TESTS_REQUIRE = [
 ]
 
 setup(name='zope.ramcache',
-      version='3.1.dev0',
+      version='3.1',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.dev',
       description='Zope RAM Cache',
